ae0203b03c6f02eddc494bd54624fc3db0a94260
andela-tfowotade <temitope.fowotade@andela.com>
2019-09-23 01:08:06 +0100

    postgresql

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User', type: :feature do
   scenario 'index page' do
-    visit user_path
+    visit root_path
     expect(page).to have_content('Hello, world!')
   end
 end
