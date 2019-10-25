1e4f715390bbabba99501eb5fceb99236a14445f
andela-tfowotade <temitope.fowotade@andela.com>
2019-10-25 01:35:31 +0100

    add sendgrid setup

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe UserMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Password Reset")
       expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["info@thehavennation.org"])
     end
 
     it "renders the body" do
